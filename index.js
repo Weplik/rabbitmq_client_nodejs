5d3dab01286c0ee74f12541739a158f401de640f
Weplik <toolpar.project@gmail.com>
2019-10-16 22:33:26 +0600

    start createChannel method

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ class RabbitmqClient extends EventEmitter {
   async openConnection() {
     try {
       if ([1, 2].includes(this[conn].status)) {
-        return;
+        return this[conn];
       }
 
       this[conn].status = 1;
@@ -47,8 +47,8 @@ class RabbitmqClient extends EventEmitter {
 
         if (this[conn].isNeedReconnect) {
           this[conn].reconnectInterval = setInterval(() => {
-            this.openConnection().then(() => {
-              if (this[conn].status === 2) {
+            this.openConnection().then(({ status }) => {
+              if (status === 2) {
                 clearInterval(this[conn].reconnectInterval);
                 this[conn].reconnectInterval = null;
               }
@@ -56,8 +56,54 @@ class RabbitmqClient extends EventEmitter {
           }, 10 * 1000);
         }
       });
+
+      return this[conn];
     } catch (err) {
       this[conn].status = 3;
+      throw err;
+    }
+  }
+
+  async createChannel(name, options = {}) {
+    try {
+      const isExistChannel =
+        this[channels][name] && [1, 2].includes(this[channels][name].status);
+
+      if (isExistChannel) {
+        return this[channels][name];
+      }
+
+      if (this[conn].status !== 2) {
+        throw new Error('Not found connection');
+      }
+
+      if (!this[channels][name]) {
+        this[channels][name] = {
+          channel: null,
+          status: 0,
+          isNeedRecreate: !!options.isNeedRecreate,
+          reconnectInterval: null,
+          isConfirmChannel: !!options.isConfirmChannel,
+        };
+      }
+
+      this[channels][name].status = 1;
+
+      if (this[channels][name].isConfirmChannel) {
+        this[channels][name].channel = await this[
+          conn
+        ].connection.createConfirmChannel();
+      } else {
+        this[channels][name].channel = await this[
+          conn
+        ].connection.createChannel();
+      }
+
+      this[channels][name].status = 2;
+
+      return this[channels][name];
+    } catch (err) {
+      this[channels][name].status = 3;
       throw err;
     }
   }
